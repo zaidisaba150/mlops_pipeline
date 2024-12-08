34b58d3061e77e19009bf051c8831f37ab6cd98c
zaidisaba150 <zaidi.saba150@gmail.com>
2024-12-08 23:57:45 +0530

    mlops pipleline 1

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -6,8 +6,11 @@ from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 import string
 import nltk
+
 nltk.download('stopwords')
 nltk.download('punkt')
+
+
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
